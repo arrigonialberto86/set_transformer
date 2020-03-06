af21f6b816a86f99273ce93de56c4b1965c3407a
arrigon5 <alberto.arrigoni@generali.com>
2020-03-06 10:55:51 +0100

    wip

--- set_transformer/model.py
+++ set_transformer/model.py
@@ -3,10 +3,10 @@ from set_transformer.layers.blocks import STEncoderBasic, STDecoderBasic
 
 
 class SetTransformer(tf.keras.Model):
-    def __init__(self, ):
+    def __init__(self, encoder_d=4, m=3, encoder_h=2, out_dim=1, decoder_d=4, decoder_h=2, k=2):
         super(SetTransformer, self).__init__()
-        self.basic_encoder = STEncoderBasic(d=4, m=3, h=2)
-        self.basic_decoder = STDecoderBasic(out_dim=1, d=4, m=2, h=2, k=2)
+        self.basic_encoder = STEncoderBasic(d=encoder_d, m=m, h=encoder_h)
+        self.basic_decoder = STDecoderBasic(out_dim=out_dim, d=decoder_d, h=decoder_h, k=k)
 
     def call(self, x):
         enc_output = self.basic_encoder(x)  # (batch_size, set_len, d_model)
